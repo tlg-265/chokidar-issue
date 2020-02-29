ae2d6f8c5cedef20ff91ac6e2149d96c61f565fc
Angel Hernandez <angel@theleadgroup.com>
2020-02-29 10:27:34 -0600

    added extra debugging logs

--- profile-watcher.js
+++ profile-watcher.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const path = require('path');
 const colors = require('colors/safe');
 const chokidar = require('chokidar');
 
@@ -7,12 +8,16 @@ console.log(`Current Profile: ${colors.red.bgBrightYellow(path_file)}`);
 
 let profile_before = {};
 
-chokidar.watch(path_file).on('change', async (path) => {
+chokidar.watch(path_file).on('change', async (path_changed) => {
 
   console.log();
-  console.log(`${colors.blue.bgYellow(`->`)} Profile changed: ${path}`);
+  console.log(`${colors.blue.bgYellow(`->`)} Profile changed: ${path_changed}`);
+  const cwd = process.cwd();
+  console.log(`cwd: ${cwd}`);
+  const filepath = path.join(cwd, path_changed);
+  console.log(`file "${filepath}" exists:`, fs.existsSync(filepath));
 
-  fs.readFile(path, (err, profile_json) => {
+  fs.readFile(path_changed, 'utf8', (err, profile_json) => {
     console.log(`->${profile_json}<-`);
     let profile = JSON.parse(profile_json);
     if (JSON.stringify(profile) != JSON.stringify(profile_before)) {
