68e7866182fd7b46796995b2e12860f7b1a358e3
Angel Hernandez <angel@theleadgroup.com>
2020-02-29 20:55:28 -0600

    fixed issue

--- profile-watcher.js
+++ profile-watcher.js
@@ -1,29 +1,39 @@
 const fs = require('fs-extra');
-const path = require('path');
 const colors = require('colors/safe');
 const chokidar = require('chokidar');
+const sleep = require('sleep');
 
 const path_file = `profiles/bill-gates.json`;
 console.log(`Current Profile: ${colors.red.bgBrightYellow(path_file)}`);
 
-let profile_before = {};
+let profile_before = fs.readFileSync(path_file).toString();
 
 chokidar.watch(path_file).on('change', async (path_changed) => {
-
-  console.log();
-  console.log(`${colors.blue.bgYellow(`->`)} Profile changed: ${path_changed}`);
-  const cwd = process.cwd();
-  console.log(`cwd: ${cwd}`);
-  const filepath = path.join(cwd, path_changed);
-  console.log(`file "${filepath}" exists:`, fs.existsSync(filepath));
-
-  fs.readFile(path_changed, 'utf8', (err, profile_json) => {
-    console.log(`->${profile_json}<-`);
-    let profile = JSON.parse(profile_json);
-    if (JSON.stringify(profile) != JSON.stringify(profile_before)) {
-      console.log('The profile has changed.');
+  let profile = fs.readFileSync(path_changed).toString();
+  if (IsValidJson(profile)) {
+    if (profile != profile_before) {
+      console.log();
+      console.log(`Profile changed: ${colors.red.bgBrightYellow(path_changed)}`);
+      process_profile(profile);
       profile_before = profile;
     }
-  });
-
+  }
+  else {
+    sleep.msleep(100);
+  }
 });
+
+function process_profile(profile_json) {
+  const profile = JSON.parse(profile_json);
+  console.log(`${profile_json}`);
+  console.log(profile.name);
+}
+
+function IsValidJson(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
